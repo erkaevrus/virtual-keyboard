d4ff965242024b5a2332f769d970254354021b5d
erkaevrus <erk_08@mail.ru>
2023-04-28 14:39:15 +0500

    add structure js for keyboard object

---
+++ src/js/index.js
@@ -0,0 +1,48 @@
+const Keyboard = {
+    elements: {
+        title: null,
+        textarea: null,
+        main: null,
+        keysContainer: null,
+        keys: []
+    },
+
+    eventHandlers: {
+        oninput: null,
+        onclose: null
+    },
+
+    properties: {
+        value: "",
+        capsLock: false,
+    },
+
+    init() {
+
+    },
+
+    _createKeys() {
+
+    },
+
+    _triggerEvent(handlerName) {
+
+    },
+
+    _toggleCapsLock() {
+
+    },
+
+    open(initialValue, oninput, onclose) {
+
+    },
+
+    close() {
+
+    }
+}
+
+
+window.addEventListener("DOMContentLoaded", function () {
+    Keyboard.init()
+})
